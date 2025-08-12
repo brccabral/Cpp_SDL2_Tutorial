539f1d90aac4010989c17814e8fc4b245c929b38
Bruno Cabral <cabral.brc@gmail.com>
2025-08-12 00:55:30 -0700

    integrate glad

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # SDL2 Tutorial
 
 Tutorial from Mike Shah https://www.youtube.com/watch?list=PLvv0ScY6vfd-p1gSnbQhY7vMe2rng0IL0
+
+## glad setup
+
+Clone `glad` and install requirements.txt
+
+```shell
+git clone --depth=1 https://github.com/Dav1dde/glad.git
+cd glad
+uv venv
+. .venv/bin/activate
+uv pip install -r requirements.txt
+```
+
+Edit file `./utility/examples.sh`, comment all `start/end` and uncomment only `gl_sdl2.c`.  
+Run `./utility/examples.sh`. It will generate a folder `build`, compile and run the example. Close the window, copy the
+`build` contents into this project's `extras/glad` folder.  
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <glad/gl.h>
 #include <SDL2/SDL.h>
 
 int main()
@@ -8,17 +9,30 @@ int main()
         return 1;
     }
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     SDL_Window *window = SDL_CreateWindow(
             "C++ SDL2 Window",
             20,
             20,
             640,
             480,
-            SDL_WINDOW_SHOWN);
+            SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    const int version = gladLoadGL((GLADloadfunc) SDL_GL_GetProcAddress);
+    printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
 
     bool gameIsRunning = true;
     while (gameIsRunning)
     {
+        glViewport(0, 0, 640, 480);
         SDL_Event event;
         // Start our event loop
         while (SDL_PollEvent(&event))
@@ -29,6 +43,11 @@ int main()
                 gameIsRunning = false;
             }
         }
+
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
     }
 
     SDL_DestroyWindow(window);
