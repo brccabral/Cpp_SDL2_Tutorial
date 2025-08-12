2bbe7dbf36c2f0aae7ba4414acda9bb90099cb4a
Bruno Cabral <cabral.brc@gmail.com>
2025-08-11 19:08:49 -0700

    open/close SDL2 window

---
+++ README.md
@@ -0,0 +1,3 @@
+# SDL2 Tutorial
+
+Tutorial from Mike Shah https://www.youtube.com/watch?list=PLvv0ScY6vfd-p1gSnbQhY7vMe2rng0IL0
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+            "C++ SDL2 Window",
+            20,
+            20,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+
+    bool gameIsRunning = true;
+    while (gameIsRunning)
+    {
+        SDL_Event event;
+        // Start our event loop
+        while (SDL_PollEvent(&event))
+        {
+            // Handle each specific event
+            if (event.type == SDL_QUIT)
+            {
+                gameIsRunning = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
