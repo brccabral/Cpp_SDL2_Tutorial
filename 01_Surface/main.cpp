c06cde7ddaaaef3bc037c620d783884f75cf866e
Bruno Cabral <cabral.brc@gmail.com>
2025-08-13 10:24:41 -0700

    Surface init

---
+++ 01_Surface/main.cpp
@@ -0,0 +1,51 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+            "C++ SDL2 Window",
+            20,
+            20,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    bool gameIsRunning = true;
+    while (gameIsRunning)
+    {
+        SDL_Event event;
+        // Start our event loop
+        while (SDL_PollEvent(&event))
+        {
+            // Handle each specific event
+            if (event.type == SDL_QUIT)
+            {
+                gameIsRunning = false;
+            }
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+                {
+                    gameIsRunning = false;
+                }
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
