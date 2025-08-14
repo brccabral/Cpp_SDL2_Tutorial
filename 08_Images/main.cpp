e1415390528c340b4fa848e51d45ef5a138599f0
Bruno Cabral <cabral.brc@gmail.com>
2025-08-14 09:36:52 -0700

    08_Images init

---
+++ 08_Images/main.cpp
@@ -0,0 +1,94 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+            "C++ SDL2 Window",
+            20,
+            20,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    constexpr int FPS = 60;
+    constexpr int frameDelay = 1000 / FPS;
+
+    bool gameIsRunning = true;
+
+    Uint64 now = SDL_GetPerformanceCounter();
+    Uint64 last = 0;
+    double deltaTime = 0.0; // seconds
+
+    while (gameIsRunning)
+    {
+        last = now;
+        now = SDL_GetPerformanceCounter();
+        deltaTime = (double) ((now - last) * 1000) / SDL_GetPerformanceFrequency();
+        deltaTime /= 1000.0; // convert ms to seconds
+
+        SDL_Event event;
+        int mouseX, mouseY;
+        Uint32 buttons = SDL_GetMouseState(&mouseX, &mouseY);
+
+        // Input
+        // Start our event loop
+        while (SDL_PollEvent(&event))
+        {
+            // Handle each specific event
+            if (event.type == SDL_QUIT)
+            {
+                gameIsRunning = false;
+            }
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+                {
+                    gameIsRunning = false;
+                }
+            }
+        }
+
+        // Draw
+        // clear screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0xFF, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        // show renderer
+        SDL_RenderPresent(renderer);
+
+        // FPS
+        const Uint64 frameTime = ((SDL_GetPerformanceCounter() - now) * 1000) /
+                                 SDL_GetPerformanceFrequency();
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay((Uint32) (frameDelay - frameTime));
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
