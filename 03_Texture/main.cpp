688248e5eac288f487f88b6c0827b8cc02e6e699
Bruno Cabral <cabral.brc@gmail.com>
2025-08-13 20:08:42 -0700

    SDL Texture init

---
+++ 03_Texture/main.cpp
@@ -0,0 +1,83 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+            "C++ SDL2 Window",
+            20,
+            20,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Rect rect = {50, 50, 150, 150};
+
+    bool gameIsRunning = true;
+    while (gameIsRunning)
+    {
+        SDL_Event event;
+        int mouseX, mouseY;
+        Uint32 buttons = SDL_GetMouseState(&mouseX, &mouseY);
+
+        // Start our event loop
+        while (SDL_PollEvent(&event))
+        {
+            // Handle each specific event
+            if (event.type == SDL_QUIT)
+            {
+                gameIsRunning = false;
+            }
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+                {
+                    gameIsRunning = false;
+                }
+            }
+            if (event.button.button == SDL_BUTTON_LEFT)
+            {
+            }
+        }
+
+        // clear screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        // draw stuff
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(renderer, 10, 10, mouseX, mouseY);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawRect(renderer, &rect);
+
+        // show renderer
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
